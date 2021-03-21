d02afc07bf32874a29a9d978a712ae3a1738fdd0
Andrea Avallone <avalloneandrea@gmail.com>
2021-03-21 19:29:56 +0100

    Add Swagger support

--- src/main/java/demetra/plan/PlanResource.java
+++ src/main/java/demetra/plan/PlanResource.java
@@ -1,6 +1,9 @@
 package demetra.plan;
 
 import demetra.domain.Recipe;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -8,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/plan")
+@Tag(name = "Plan Resource", description = "Operations about meal plans")
 public class PlanResource {
 
     @Inject
@@ -15,7 +19,8 @@ public class PlanResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Recipe> getPlan(@QueryParam("days") @DefaultValue("7") int days) {
+    @Operation(summary = "Build a meal plan given the number of days to cover")
+    public List<Recipe> getPlan(@Parameter(description = "Number of days to cover") @QueryParam("days") @DefaultValue("7") int days) {
         return service.getPlan(days);
     }
 
